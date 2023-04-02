a048d4da1ad397271ef770db51c297c67d4f7696
nmhung04 <101931793+nmhung04@users.noreply.github.com>
2023-04-02 22:20:12 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,60 @@
-#include <iostream>
+//GAME 2048
+#include <SDL.h>
+#include <stdio.h>
 
-using namespace std;
+//Screen dimension constants
+const int SCREEN_WIDTH = 320*3;
+const int SCREEN_HEIGHT = 240*3;
 
-int main()
+
+int main( int argc, char* args[] )
 {
-    cout << "Hello world!" << endl;
+    //The window we'll be rendering to
+    SDL_Window* window = NULL;
+
+    //The surface contained by the window
+    SDL_Surface* screenSurface = NULL;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    }
+    else
+    {
+        //Create window
+        window = SDL_CreateWindow( "2048", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( window == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        }
+        else
+        {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface( window );
+
+            //Fill the surface white
+            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+
+            //Update the surface
+            SDL_UpdateWindowSurface( window );
+
+            //Hack to get window to stay up
+            SDL_Event e;
+            bool quit = false;
+            while( quit == false )
+            {
+                while( SDL_PollEvent( &e ) )
+                {
+                    if( e.type == SDL_QUIT ) quit = true;
+                }
+            }
+        }
+    }    //Destroy window
+    SDL_DestroyWindow( window );
+
+    //Quit SDL subsystems
+    SDL_Quit();
+
     return 0;
 }
